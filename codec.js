ee9d35761bfb6342b91f7eeb1d5c4a37d01840f2
Ditmar Castro <ditmarcastro@gmail.com>
2018-09-06 17:52:58 -0400

    decode ready

--- codec.js
+++ codec.js
@@ -7,3 +7,18 @@ function code (msn) {
   }
   return data.toString().replace(/,/g, "");
 }
+var msncode = "486f6c61206d756e646f206573746f20657320756e6120707275656261";
+console.log(decode(msncode));
+function decode (msncode) {
+	var data = []
+	for (var i = 0; i < msncode.length; i += 2) {
+		data.push(msncode.substr(i, 2));
+	}
+	var cad = "";
+	for (var i = 0; i < data.length; i ++) {
+		var decimal = parseInt(data[i], 16);
+		cad += String.fromCharCode(decimal);
+	}
+	
+	return cad;
+}
